2d00923817799f23edb17059321ff61fcfba47d3
Bruce Shields <bwshields@sbcglobal.net>
2013-07-04 18:19:01 -0400

    Create main.py
    First working portfolio with jinja2

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import webapp2
+
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+    
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'name': 'SomeGuy',
+            'verb': 'extremely enjoy'
+        }
+
+        template = jinja_env.get_template('index.html')
+      self.response.out.write(template.render(template_values))
+app = webapp2.WSGIApplication(routes=[ 
+    ( r'/', MainPage ),
+    # ... other paths ...
+], debug=True)

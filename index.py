77d3d534967c29af37824deae470b3dcfd0bbdb5
Bruce Shields <bwshields@sbcglobal.net>
2013-07-07 21:40:13 -0400

    Update index.py

--- index.py
+++ index.py
@@ -4,17 +4,18 @@ import os
 import webapp2
 
 import jinja2
-from jinja2 import Environment, PackageLoader
-env = Environment(loader = PackageLoader('index', 'templates'))
-template = env.get_template('base.html')
+from jinja2 import Environment, PackageLoader,FileSystemLoader, Template
+env = Environment(loader = PackageLoader('index', 'templates/'))
+
+
                                
 class MainPage(webapp2.RequestHandler):
                                
     def get(self):
-                               temp = env.get_template('index.html')
+    	temp = env.get_template('index.html')
     	#self.response.headers['Content-Type'] = 'text/plain'
     	self.response.out.write(temp.render())
-
+	
 app = webapp2.WSGIApplication(routes=[ 
     ( r'/', MainPage ),
 ], debug=True)
